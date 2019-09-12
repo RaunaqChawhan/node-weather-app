5fd0ad26c038a5edac0af380057fd1bd19665c18
New <chauhanraunaq878@gmail.com>
2019-09-13 00:52:46 +0530

    forecast function

--- app.js
+++ app.js
@@ -1,43 +1,49 @@
 const request = require('request')
 const yargs = require('yargs')
 const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const url = 'https://api.darksky.net/forecast/your_key/37.8267,-122.4233?units=us'
-let address
+// const url = 'https://api.darksky.net/forecast/your_key/37.8267,-122.4233?units=us'
+// let address
 
-request({ url: url, json: true }, (error, response) => {
-    //const data = JSON.parse(response.body) //setting json to true automatically 
-    //parses the json and no need to include this line
-    //console.log(response.body.currently)
-    if(error) {
-        console.log('Unable to connect to weather service')
-    } else if(response.body.error) {
-        console.log('Unable to find location')
-    } else {
-        console.log(response.body.daily.data[0].summary + ' It is currently ' + 
-    response.body.currently.temperature + ' degrees out. There is a ' + 
-    response.body.currently.precipProbability + '% chance of rain.')
-    } 
-})
+// request({ url: url, json: true }, (error, response) => {
+//     //const data = JSON.parse(response.body) //setting json to true automatically 
+//     //parses the json and no need to include this line
+//     //console.log(response.body.currently)
+//     if(error) {
+//         console.log('Unable to connect to weather service')
+//     } else if(response.body.error) {
+//         console.log('Unable to find location')
+//     } else {
+//         console.log(response.body.daily.data[0].summary + ' It is currently ' + 
+//     response.body.currently.temperature + ' degrees out. There is a ' + 
+//     response.body.currently.precipProbability + '% chance of rain.')
+//     } 
+// })
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=your_key&limit=1'
+// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=your_key&limit=1'
 
-request({url: geocodeURL, json: true}, (error, response) => {
-    //console.log(response.body.features[0].center)
-    if(error) {
-        console.log('Unable to connect to location services!')
-    } else if(response.body.features.length === 0) {
-        console.log('Unable to find location. Try another search.')
-    } else {
-        const latitude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0]
-        console.log(latitude, longitude)  
-    }
-})
+// request({url: geocodeURL, json: true}, (error, response) => {
+//     //console.log(response.body.features[0].center)
+//     if(error) {
+//         console.log('Unable to connect to location services!')
+//     } else if(response.body.features.length === 0) {
+//         console.log('Unable to find location. Try another search.')
+//     } else {
+//         const latitude = response.body.features[0].center[1]
+//         const longitude = response.body.features[0].center[0]
+//         console.log(latitude, longitude)  
+//     }
+// })
 
 
 
 geocode('New York', (error, data) => {  //defining callback the way most libraries define it (1st arg: error, 2nd: data)
+    console.log('Error', error)
+    console.log('Data', data)
+})
+
+forecast(-75.7088, 44.1545, (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
 })
